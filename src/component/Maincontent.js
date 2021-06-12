1704ab67bafc87ca20d77e2e29e0a3fb416e43f2
DongMZ1 <915094594@qq.com>
2021-06-11 20:57:15 -0400

    try to fix bug

--- src/component/Maincontent.js
+++ src/component/Maincontent.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import "firebase/firestore";
-import { useFirestoreCollection, useFirestore } from "reactfire";
+import {useFirestoreCollection, useFirestore } from "reactfire";
 
 const Maincontent = () =>{
-    const usersref = useFirestore().collection("users");
+    const usersref = useFirestore().collection("items");
     
     const {data, status} = useFirestoreCollection(usersref);
-    
-    if(status === 'loading') return <>is Loading ..........</>;
-
+    if (status === 'loading') {
+      return <p>Fetching burrito flavor...</p>;
+    }
     return <>
-      {data?.docs?.map(user => <h1>{user.data().name}</h1>)}
+      {data?.docs?.map(d =><h1>{d.data().name}</h1>)}
     </>;
 }
 
